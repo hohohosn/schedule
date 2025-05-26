e0218cb6db24081b5b64fe78132a97fae12b280d
hohohosn <day7of01@gmail.com>
2025-05-26 17:24:07 +0900

    레포지터리

---
+++ src/main/java/com/example/schedulepj/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.schedulepj.repository;
+
+import com.example.schedulepj.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
